13678944a5357f2edb45a3a9553338d8a1163c12
Jason Lavoie <jlavoie@bowdoin.edu>
2021-02-18 09:10:34 -0500

    add cloudfront distribution

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,51 @@
+data "aws_acm_certificate" "cert" {
+  domain = "*.oasys.net"
+}
+
+resource "aws_cloudfront_distribution" "hugo" {
+  origin {
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    domain_name = aws_s3_bucket.hugo.website_endpoint
+    origin_id   = aws_s3_bucket.hugo.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.hugo.id
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,3 @@
 output "s3_url" {
-  value = aws_s3_bucket.hugo.bucket_domain_name
+  value = aws_s3_bucket.hugo.website_endpoint
 }
